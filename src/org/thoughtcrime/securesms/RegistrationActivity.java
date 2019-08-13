a6087b1503c062873cdb51e4cadf4d01cd8990db
f.harms@haylee <f.harms@devcups.com>
2019-08-13 10:52:21 +0100

    pick + fix 3b67382f6772419e91044dc86aa0ee194d86bdea

--- src/org/thoughtcrime/securesms/RegistrationActivity.java
+++ src/org/thoughtcrime/securesms/RegistrationActivity.java
@@ -40,9 +40,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dd.CircularProgressButton;
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.auth.api.phone.SmsRetrieverClient;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.tasks.Task;
 import com.google.i18n.phonenumbers.AsYouTypeFormatter;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
@@ -62,18 +67,22 @@ import org.raapp.messenger.crypto.AttachmentSecretProvider;
 import org.raapp.messenger.crypto.IdentityKeyUtil;
 import org.raapp.messenger.crypto.PreKeyUtil;
 import org.raapp.messenger.crypto.SessionUtil;
+//import org.raapp.messenger.crypto.UnidentifiedAccessUtil;
 import org.raapp.messenger.database.Address;
 import org.raapp.messenger.database.DatabaseFactory;
 import org.raapp.messenger.database.IdentityDatabase;
 import org.raapp.messenger.database.NoExternalStorageException;
 import org.raapp.messenger.jobs.DirectoryRefreshJob;
 import org.raapp.messenger.jobs.GcmRefreshJob;
+//import org.raapp.messenger.jobs.RotateCertificateJob;
 import org.raapp.messenger.lock.RegistrationLockReminders;
+//import org.raapp.messenger.logging.Log;
 import org.raapp.messenger.notifications.NotificationChannels;
 import org.raapp.messenger.permissions.Permissions;
 import org.raapp.messenger.push.AccountManagerFactory;
 import org.raapp.messenger.service.DirectoryRefreshListener;
 import org.raapp.messenger.service.RotateSignedPreKeyListener;
+import org.raapp.messenger.service.VerificationCodeParser;
 import org.raapp.messenger.util.BackupUtil;
 import org.raapp.messenger.util.DateUtils;
 import org.raapp.messenger.util.Dialogs;
@@ -94,6 +103,8 @@ import org.whispersystems.signalservice.api.util.PhoneNumberFormatter;
 import org.whispersystems.signalservice.internal.push.LockedException;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
@@ -105,12 +116,12 @@ import java.util.concurrent.TimeUnit;
  * @author Moxie Marlinspike
  *
  */
-public class RegistrationActivity extends BaseActionBarActivity implements VerificationCodeView.OnCodeEnteredListener {
+public class RegistrationActivity extends org.raapp.messenger.BaseActionBarActivity implements VerificationCodeView.OnCodeEnteredListener {
 
   private static final int    PICK_COUNTRY              = 1;
   private static final int    SCENE_TRANSITION_DURATION = 250;
-  public static final  String CHALLENGE_EVENT           = "org.raapp.messenger.CHALLENGE_EVENT";
-  public static final  String CHALLENGE_EXTRA           = "CAAChallenge";
+  private static final int    DEBUG_TAP_TARGET          = 8;
+  private static final int    DEBUG_TAP_ANNOUNCE        = 4;
   public static final  String RE_REGISTRATION_EXTRA     = "re_registration";
 
   private static final String TAG = RegistrationActivity.class.getSimpleName();
@@ -121,7 +132,6 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
   private TextView               countryCode;
   private TextView               number;
   private CircularProgressButton createButton;
-  private TextView               termsLinkView;
   private TextView               informationView;
   private TextView               informationToggleText;
   private TextView               title;
@@ -146,7 +156,7 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
   private VerificationPinKeyboard     keyboard;
   private VerificationCodeView        verificationCodeView;
   private RegistrationState           registrationState;
-  private ChallengeReceiver           challengeReceiver;
+  private SmsRetrieverReceiver        smsRetrieverReceiver;
   private SignalServiceAccountManager accountManager;
 
 
@@ -261,7 +271,7 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
     });
     this.countrySpinner.setOnKeyListener((v, keyCode, event) -> {
       if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER && event.getAction() == KeyEvent.ACTION_UP) {
-        Intent intent = new Intent(RegistrationActivity.this, CountrySelectionActivity.class);
+        Intent intent = new Intent(RegistrationActivity.this, org.raapp.messenger.CountrySelectionActivity.class);
         startActivityForResult(intent, PICK_COUNTRY);
         return true;
       }
@@ -294,7 +304,7 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
     Permissions.with(RegistrationActivity.this)
                .request(Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CALL_LOG) // , Manifest.permission.PROCESS_OUTGOING_CALLS
+                        Manifest.permission.READ_PHONE_STATE)
                .ifNecessary()
                .withRationaleDialog(getString(R.string.RegistrationActivity_signal_needs_access_to_your_contacts_and_media_in_order_to_connect_with_friends),
                                     R.drawable.ic_contacts_white_48dp, R.drawable.ic_folder_white_48dp)
@@ -428,31 +438,12 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
       return;
     }
 
-    Permissions.with(this)
-               .request(Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS)
-               .ifNecessary()
-               .withRationaleDialog(getString(R.string.RegistrationActivity_to_easily_verify_your_phone_number_signal_can_automatically_detect_your_verification_code), R.drawable.ic_textsms_white_48dp)
-               .onAnyResult(this::handleRegisterWithPermissions)
-               .execute();
-  }
-
-  private void handleRegisterWithPermissions() {
-    if (TextUtils.isEmpty(countryCode.getText())) {
-      Toast.makeText(this, getString(R.string.RegistrationActivity_you_must_specify_your_country_code), Toast.LENGTH_LONG).show();
-      return;
-    }
-
-    if (TextUtils.isEmpty(number.getText())) {
-      Toast.makeText(this, getString(R.string.RegistrationActivity_you_must_specify_your_phone_number), Toast.LENGTH_LONG).show();
-      return;
-    }
-
     final String e164number = getConfiguredE164Number();
 
     if (!PhoneNumberFormatter.isValidNumber(e164number)) {
-      Dialogs.showAlertDialog(this, getString(R.string.RegistrationActivity_invalid_number),
-                              String.format(getString(R.string.RegistrationActivity_the_number_you_specified_s_is_invalid),
-                                            e164number));
+      Dialogs.showAlertDialog(this,
+                              getString(R.string.RegistrationActivity_invalid_number),
+                              String.format(getString(R.string.RegistrationActivity_the_number_you_specified_s_is_invalid), e164number));
       return;
     }
 
@@ -470,11 +461,30 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
     }
   }
 
-  @SuppressLint("StaticFieldLeak")
   private void handleRequestVerification(@NonNull String e164number, boolean gcmSupported) {
     createButton.setIndeterminateProgressMode(true);
     createButton.setProgress(50);
 
+    if (gcmSupported) {
+      SmsRetrieverClient client = SmsRetriever.getClient(this);
+      Task<Void>         task   = client.startSmsRetriever();
+
+      task.addOnSuccessListener(none -> {
+        Log.i(TAG, "Successfully registered SMS listener.");
+        requestVerificationCode(e164number, true, true);
+      });
+
+      task.addOnFailureListener(e -> {
+        Log.w(TAG, "Failed to register SMS listener.", e);
+        requestVerificationCode(e164number, true, false);
+      });
+    } else {
+      requestVerificationCode(e164number, false, false);
+    }
+  }
+
+  @SuppressLint("StaticFieldLeak")
+  private void requestVerificationCode(@NonNull String e164number, boolean gcmSupported, boolean smsRetrieverSupported) {
     new AsyncTask<Void, Void, Pair<String, Optional<String>>> () {
       @Override
       protected @Nullable Pair<String, Optional<String>> doInBackground(Void... voids) {
@@ -492,6 +502,7 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
           }
 
           accountManager = AccountManagerFactory.createManager(RegistrationActivity.this, e164number, password);
+          // accountManager.requestSmsVerificationCode(smsRetrieverSupported); // org.whispersystems:signal-service-android:2.12.4
           accountManager.requestSmsVerificationCode();
 
           return new Pair<>(password, gcmToken);
@@ -515,20 +526,32 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
     }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
   }
 
-  private void handleChallengeReceived(@Nullable String challenge) {
-    if (challenge != null && challenge.length() == 6 && registrationState.state == RegistrationState.State.VERIFYING) {
-      verificationCodeView.clear();
+  private void handleVerificationCodeReceived(@Nullable String code) {
+    List<Integer> parsedCode = convertVerificationCodeToDigits(code);
 
-      try {
-        for (int i=0;i<challenge.length();i++) {
-          final int index = i;
-          verificationCodeView.postDelayed(() -> verificationCodeView.append(Integer.parseInt(Character.toString(challenge.charAt(index)))), i * 200);
-        }
-      } catch (NumberFormatException e) {
-        Log.w(TAG, e);
-        verificationCodeView.clear();
-      }
+    for (int i = 0; i < parsedCode.size(); i++) {
+      int index = i;
+      verificationCodeView.postDelayed(() -> verificationCodeView.append(parsedCode.get(index)), i * 200);
     }
+  }
+
+  private List<Integer> convertVerificationCodeToDigits(@Nullable String code) {
+    if (code == null || code.length() != 6 || registrationState.state != RegistrationState.State.VERIFYING) {
+      return Collections.emptyList();
+    }
+
+    List<Integer> result = new LinkedList<>();
+
+    try {
+      for (int i = 0; i < code.length(); i++) {
+        result.add(Integer.parseInt(Character.toString(code.charAt(i))));
+      }
+    } catch (NumberFormatException e) {
+      Log.w(TAG, "Failed to convert code into digits.",e );
+      return Collections.emptyList();
+    }
+
+    return result;
   }
 
   @SuppressLint("StaticFieldLeak")
@@ -708,7 +731,7 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
   }
 
   private void handleSuccessfulRegistration() {
-    ApplicationContext.getInstance(RegistrationActivity.this).getJobManager().add(new DirectoryRefreshJob(RegistrationActivity.this, false));
+    org.raapp.messenger.ApplicationContext.getInstance(RegistrationActivity.this).getJobManager().add(new DirectoryRefreshJob(RegistrationActivity.this, false));
 
     DirectoryRefreshListener.schedule(RegistrationActivity.this);
     RotateSignedPreKeyListener.schedule(RegistrationActivity.this);
@@ -716,7 +739,7 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
     Intent nextIntent = getIntent().getParcelableExtra("next_intent");
 
     if (nextIntent == null) {
-      nextIntent = new Intent(RegistrationActivity.this, ConversationListActivity.class);
+      nextIntent = new Intent(RegistrationActivity.this, org.raapp.messenger.ConversationListActivity.class);
     }
 
     startActivity(nextIntent);
@@ -963,7 +986,7 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
     Intent nextIntent = getIntent().getParcelableExtra("next_intent");
 
     if (nextIntent == null) {
-      nextIntent = new Intent(RegistrationActivity.this, ConversationListActivity.class);
+      nextIntent = new Intent(RegistrationActivity.this, org.raapp.messenger.ConversationListActivity.class);
     }
 
     startActivity(nextIntent);
@@ -980,15 +1003,15 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
   }
 
   private void initializeChallengeListener() {
-    challengeReceiver = new ChallengeReceiver();
-    IntentFilter filter = new IntentFilter(CHALLENGE_EVENT);
-    registerReceiver(challengeReceiver, filter);
+    smsRetrieverReceiver = new SmsRetrieverReceiver();
+    IntentFilter filter = new IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION);
+    registerReceiver(smsRetrieverReceiver, filter);
   }
 
   private void shutdownChallengeListener() {
-    if (challengeReceiver != null) {
-      unregisterReceiver(challengeReceiver);
-      challengeReceiver = null;
+    if (smsRetrieverReceiver != null) {
+      unregisterReceiver(smsRetrieverReceiver);
+      smsRetrieverReceiver = null;
     }
   }
 
@@ -1015,11 +1038,32 @@ public class RegistrationActivity extends BaseActionBarActivity implements Verif
     else                       restoreBackupProgress.setText(getString(R.string.RegistrationActivity_d_messages_so_far, event.getCount()));
   }
 
-  private class ChallengeReceiver extends BroadcastReceiver {
+  private class SmsRetrieverReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-      Log.i(TAG, "Got a challenge broadcast...");
-      handleChallengeReceived(intent.getStringExtra(CHALLENGE_EXTRA));
+      Log.i(TAG, "SmsRetrieverReceiver received a broadcast...");
+
+      if (SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.getAction())) {
+        Bundle extras = intent.getExtras();
+        Status status = (Status) extras.get(SmsRetriever.EXTRA_STATUS);
+
+        switch (status.getStatusCode()) {
+          case CommonStatusCodes.SUCCESS:
+            Optional<String> code = VerificationCodeParser.parse(context, (String) extras.get(SmsRetriever.EXTRA_SMS_MESSAGE));
+            if (code.isPresent()) {
+              Log.i(TAG, "Received verification code.");
+              handleVerificationCodeReceived(code.get());
+            } else {
+              Log.w(TAG, "Could not parse verification code.");
+            }
+            break;
+          case CommonStatusCodes.TIMEOUT:
+            Log.w(TAG, "Hit a timeout waiting for the SMS to arrive.");
+            break;
+        }
+      } else {
+        Log.w(TAG, "SmsRetrieverReceiver received the wrong action?");
+      }
     }
   }
 
