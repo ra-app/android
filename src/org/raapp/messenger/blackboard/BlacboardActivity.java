df40c3d1ff05290740cb6ffb39b82c136fadfaa6
Fran Dioniz <frandioniz@gmail.com>
2019-11-07 21:55:31 +0000

    Call to the server to retrieve all notes for blackboard. Added progress bar

--- src/org/raapp/messenger/blackboard/BlacboardActivity.java
+++ src/org/raapp/messenger/blackboard/BlacboardActivity.java
@@ -9,15 +9,31 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.raapp.messenger.R;
+import org.raapp.messenger.client.Client;
+import org.raapp.messenger.client.datamodel.Note;
+import org.raapp.messenger.client.datamodel.Responses.ResponseBlackboardList;
 import org.raapp.messenger.conversation.ConversationTitleView;
 import org.raapp.messenger.mms.GlideApp;
 import org.raapp.messenger.mms.GlideRequests;
 import org.raapp.messenger.recipients.Recipient;
+import org.raapp.messenger.registration.InvitationActivity;
 import org.raapp.messenger.util.DynamicLanguage;
 import org.raapp.messenger.util.DynamicNoActionBarTheme;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class BlacboardActivity extends AppCompatActivity implements BlackboardInterface{
 
@@ -36,6 +52,7 @@ public class BlacboardActivity extends AppCompatActivity implements BlackboardIn
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private View mNoteDetailView;
+    private ProgressBar progressBar;
 
 
     @Override
@@ -82,6 +99,7 @@ public class BlacboardActivity extends AppCompatActivity implements BlackboardIn
         mLayoutManager = new GridLayoutManager(this,3);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mNoteDetailView = findViewById(R.id.rl_detail_pin);
+        progressBar = findViewById(R.id.progress_bar);
 
         showNoteList();
     }
@@ -91,29 +109,29 @@ public class BlacboardActivity extends AppCompatActivity implements BlackboardIn
         glideRequests = GlideApp.with(this);
         titleView.setTitle(glideRequests, recipient);
 
+        Client.buildBase(this);
+        Client.getBlackboard(new Callback<ResponseBlackboardList>() {
+            @Override
+            public void onResponse(Call<ResponseBlackboardList> call, Response<ResponseBlackboardList> response) {
+                if (response.isSuccessful()) {
+                    ResponseBlackboardList resp = response.body();
+                    if (resp != null && resp.getSuccess()) {
+                        List<Note> notes = resp.getNote();
+                        if (notes != null) {
+                            mAdapter = new BlackboardAdapter(BlacboardActivity.this, notes);
+                            mRecyclerView.setAdapter(mAdapter);
+                        }
+                    }
+                }
+                progressBar.setVisibility(View.GONE);
+            }
 
-        // Initialize a new instance of RecyclerView Adapter instance
-        String[] notes = {
-                "Aardvark",
-                "Albatross",
-                "Alligator",
-                "Alpaca",
-                "Ant",
-                "Anteater",
-                "Antelope",
-                "Ape",
-                "Armadillo",
-                "Donkey",
-                "Baboon",
-                "Badger",
-                "Barracuda",
-                "Bear",
-                "Beaver",
-                "Bee"
-        };
-        mAdapter = new BlackboardAdapter(this,notes);
-        // Set the adapter for RecyclerView
-        mRecyclerView.setAdapter(mAdapter);
+            @Override
+            public void onFailure(Call<ResponseBlackboardList> call, Throwable t) {
+                Toast.makeText(BlacboardActivity.this, "Error retrieving notes", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        }, recipient.getAddress().toString());
     }
 
     private void showNoteDetail(){
@@ -128,17 +146,29 @@ public class BlacboardActivity extends AppCompatActivity implements BlackboardIn
 
     @Override
     public void onNoteClick(Object object) {
-
-        showNoteDetail();
-
         TextView noteTitle = mNoteDetailView.findViewById(R.id.note_title);
         TextView noteBody = mNoteDetailView.findViewById(R.id.note_body);
         TextView noteDate = mNoteDetailView.findViewById(R.id.note_date);
 
-        noteDate.setText("DATEHERE H:MM");
-        //noteBody.setText('get text from object');
-        //noteTitle.setText('get title from object');
+        Note note = (Note) object;
+        noteTitle.setText(note.getTitle());
+        noteBody.setText(note.getContent());
 
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        Date myDate = null;
+        try {
+            myDate = dateFormat.parse(note.getTsCreated());
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd / HH:mm");
+        String finalDate = timeFormat.format(myDate);
+
+        noteDate.setText(finalDate);
+
+        showNoteDetail();
     }
 
     @Override
--- src/org/raapp/messenger/blackboard/BlackboardAdapter.java
+++ src/org/raapp/messenger/blackboard/BlackboardAdapter.java
@@ -6,17 +6,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import org.raapp.messenger.R;
+import org.raapp.messenger.client.datamodel.Note;
 import org.raapp.messenger.recipients.Recipient;
 
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class BlackboardAdapter extends RecyclerView.Adapter<BlackboardAdapter.ViewHolder>{
-    private String[] mDataSet;
+    private List<Note> mNotes;
     private Context mContext;
 
-    public BlackboardAdapter(Context context,String[] DataSet){
-        mDataSet = DataSet;
+    public BlackboardAdapter(Context context, List<Note> notes){
+        mNotes = notes;
         mContext = context;
     }
 
@@ -25,7 +28,7 @@ public class BlackboardAdapter extends RecyclerView.Adapter<BlackboardAdapter.Vi
         public ViewHolder(View v){
             super(v);
             mTitle = v.findViewById(R.id.note_title);
-            mBody =  v.findViewById(R.id.note_title);
+            mBody =  v.findViewById(R.id.note_body);
         }
     }
 
@@ -39,15 +42,15 @@ public class BlackboardAdapter extends RecyclerView.Adapter<BlackboardAdapter.Vi
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position){
-        holder.mTitle.setText(mDataSet[position]);
-        holder.itemView.setOnClickListener(view -> ((BlackboardInterface)mContext).onNoteClick(mDataSet[position]));
+        holder.mTitle.setText(mNotes.get(position).getTitle());
+        holder.mBody.setText(mNotes.get(position).getContent());
+        holder.itemView.setOnClickListener(view -> ((BlackboardInterface)mContext).onNoteClick(mNotes.get(position)));
     }
 
     @Override
     public int getItemCount(){
-        return mDataSet.length;
+        return mNotes.size();
     }
-
 
 }
 
